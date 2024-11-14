e99cf61e1f093cf4f75f513e79b5591f28c9b46a
Nortrix0 <minecrafternortrix@gmail.com>
2024-11-14 13:02:49 -0600

    Add Gamemode/gamescope and Snipping Shortcut

--- Desktops/KDE/Configs/Bowens/configure.sh
+++ Desktops/KDE/Configs/Bowens/configure.sh
@@ -2,3 +2,5 @@
 
 sed -i 's|Numlock=.*|Numlock=on|' /mnt/usr/lib/sddm/sddm.conf.d/default.conf
 echo "NoDisplay=true" | tee -a /mnt/usr/share/applications/{avahi-discover.desktop,bssh.desktop,bvnc.desktop,org.kde.kuserfeedback-console.desktop,org.kde.plasma.emojier.desktop,qv4l2.desktop,qvidcap.desktop}
+usermod -aG gamemode $USER
+kwriteconfig6 --file kglobalshortcutsrc --group services --group org.flameshot.Flameshot.desktop --key "Capture" "Meta+Shift+S"
